b261e46c9ecdc1be4791315dc0fc81ce1058e16b
BindleImpsbane <bindleimpsbane@gmail.com>
2013-10-04 15:03:29 -0600

    Start-up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BAUS::Application.config.secret_key_base = '2f75e02b447db7aa0c190805e01e41db17e49d8edf7921fa8aa65c13921fcb6d72133bb9f1d332195eca4326c871eff8173aa32a057ba6818f04e6de2176cb1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BAUS::Application.config.session_store :cookie_store, key: '_BAUS_session'
